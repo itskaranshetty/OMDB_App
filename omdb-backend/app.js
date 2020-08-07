ef6a1810932f07a2f881f7d339dfd4f7094561b2
itskaranshetty <f20180111@goa.bits-pilani.ac.in>
2020-08-07 15:35:33 +0530

    Restart

--- omdb-backend/app.js
+++ omdb-backend/app.js
@@ -1,21 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
+const omdbRouter = require('./routes/omdb');
 
-var indexRouter = require('./routes/index.js');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();  //Creates the express application
-
+// These will not send a response
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// These can send a response or pass it on
+// Based on url you can do one of two things
+// 1. Return a file
+// 2. Execute a function and return a response
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/omdb', omdbRouter);
 
 module.exports = app;
--- omdb-ui/src/utils/constants.js
+++ omdb-ui/src/utils/constants.js
@@ -1,5 +1,5 @@
 
-const baseUrl = 'http://localhost:3001/';
+const baseUrl = 'http://localhost:3000/';
 const axiosHeaders = {
 	headers: {
 	  'Access-Control-Allow-Origin': '*',
