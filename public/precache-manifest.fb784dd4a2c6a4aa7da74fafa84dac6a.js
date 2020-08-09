df59ab49985e38cb7e12b88a34f471efcf6b7066
itskaranshetty <f20180111@goa.bits-pilani.ac.in>
2020-08-09 22:56:24 +0530

    public

---
+++ public/precache-manifest.fb784dd4a2c6a4aa7da74fafa84dac6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac45223407af5b6c1ca9c1ef2e14880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba5fca13c5f2d5a4f58",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "262105e164a2e779cd47",
+    "url": "/static/css/main.a5136d75.chunk.css"
+  },
+  {
+    "revision": "0ba5fca13c5f2d5a4f58",
+    "url": "/static/js/2.af2d132f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af2d132f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262105e164a2e779cd47",
+    "url": "/static/js/main.8e4cfbba.chunk.js"
+  },
+  {
+    "revision": "9091f47bb88943cb9e61",
+    "url": "/static/js/runtime-main.89d020cb.js"
+  }
+]);
